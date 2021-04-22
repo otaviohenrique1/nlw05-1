d7695162f7b546e5cdd77dd13e44bed8aa33a6ce
Otavio Henrique <OtavioHenLimSil@gmail.com>
2021-04-22 16:13:54 -0300

    Commit 3 dos arquivos

--- podcastr_next/src/pages/index.tsx
+++ podcastr_next/src/pages/index.tsx
@@ -1,6 +1,7 @@
 // import { useEffect } from "react";
 import { GetStaticProps } from "next";
 import Image from "next/image";
+import Link from "next/link";
 import api from "../services/api";
 import { format, parseISO } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
@@ -11,7 +12,7 @@ type Episode = {
   id: string;
   title: string;
   thumbnail: string;
-  description: string;
+  // description: string;
   members: string;
   duration: number;
   durationAsString: string;
@@ -49,7 +50,9 @@ export default function Home({ latestEpisodes, allApisodes }: HomeProps) {
                   objectFit="cover"
                 />
                 <div className={styles.episodeDetails}>
-                  <a href="">{episode.title}</a>
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                  </Link>
                   <p>{episode.members}</p>
                   <span>{episode.publishedAt}</span>
                   <span>{episode.durationAsString}</span>
@@ -62,7 +65,51 @@ export default function Home({ latestEpisodes, allApisodes }: HomeProps) {
           })}
         </ul>
       </section>
-      <section className={styles.allEpisodes}></section>
+      <section className={styles.allEpisodes}>
+        <h2>Todos episódios</h2>
+        <table cellSpacing={0}>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Podcast</th>
+              <th>Integrantes</th>
+              <th>Data</th>
+              <th>Duração</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {allApisodes.map(episode => {
+              return (
+                <tr key={episode.id}>
+                  <td style={{ width: 72 }}>
+                    <Image
+                      width={120}
+                      height={120}
+                      src={episode.thumbnail}
+                      alt={episode.title}
+                      objectFit="cover"
+                    />
+                  </td>
+                  <td>
+                    <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                    </Link>
+                  </td>
+                  <td>{episode.members}</td>
+                  <td style={{ width: 100 }}>{episode.publishedAt}</td>
+                  <td>{episode.durationAsString}</td>
+                  <td>
+                    <button type="button">
+                      <img src="/play-green.svg" alt="Tocar episódio"/>
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </section>
     </div>
   );
 }
@@ -85,7 +132,7 @@ export const getStaticProps: GetStaticProps = async () => {
       publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR}),
       duration: Number(episode.duration),
       durationAsString: converterDurationToTimeString(Number(episode.duration)),
-      description: episode.description,
+      // description: episode.description,
       url: episode.url,
     };
   });
