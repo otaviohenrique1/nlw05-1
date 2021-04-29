97099891c72be8911e572793272b15b67e414ccf
Otavio Henrique <OtavioHenLimSil@gmail.com>
2021-04-28 22:59:09 -0300

    Commit 9 dos aquivos

--- devquiz/lib/home/widgets/appbar/app_bar_widget.dart
+++ devquiz/lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,5 +1,6 @@
 import 'package:devquiz/core/app_gradients.dart';
 import 'package:devquiz/core/app_text_styles.dart';
+import 'package:devquiz/home/widgets/score_card/score_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends PreferredSize {
@@ -8,38 +9,48 @@ class AppBarWidget extends PreferredSize {
           preferredSize: Size.fromHeight(250),
           child: Container(
             height: 250,
-            decoration: BoxDecoration(
-              gradient: AppGradients.linear,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text.rich(
-                    TextSpan(
-                        text: "Olá, ",
-                        style: AppTextStyles.title,
-                        children: [
-                          TextSpan(
-                            text: "Otavio DEV",
-                            style: AppTextStyles.titleBold,
-                          ),
-                        ]),
+            child: Stack(
+              children: [
+                Container(
+                  height: 161,
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                    gradient: AppGradients.linear,
                   ),
-                  Container(
-                    width: 58,
-                    height: 58,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      image: DecorationImage(
-                        image: NetworkImage(
-                            "https://avatars.githubusercontent.com/u/42388160?v=4"),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(
+                        TextSpan(
+                            text: "Olá, ",
+                            style: AppTextStyles.title,
+                            children: [
+                              TextSpan(
+                                text: "Otavio DEV",
+                                style: AppTextStyles.titleBold,
+                              ),
+                            ]),
                       ),
-                    ),
+                      Container(
+                        width: 58,
+                        height: 58,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                                "https://avatars.githubusercontent.com/u/42388160?v=4"),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                Align(
+                  alignment: Alignment(0.0, 1.0),
+                  child: ScoreCardWidget(),
+                ),
+              ],
             ),
           ),
         );
