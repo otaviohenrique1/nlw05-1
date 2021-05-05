f3fd4dc505341200ffd544d971ca122d977396cc
Otavio Henrique <OtavioHenLimSil@gmail.com>
2021-05-04 22:40:35 -0300

    Revert "Commit 14 dos arquivos"
    This reverts commit 006026b5d43c9c6194a66047362e94f734389bff.

--- devquiz/lib/core/app_widget.dart
+++ devquiz/lib/core/app_widget.dart
@@ -1,6 +1,5 @@
-// import 'package:devquiz/home/home_page.dart';
+import 'package:devquiz/home/home_page.dart';
 // import 'package:devquiz/splash/splash_page.dart';
-import 'package:devquiz/challenge/challenge_page.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
@@ -8,7 +7,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DevQuiz',
-      home: ChallengePage(),
+      home: HomePage(),
     );
   }
 }
--- devquiz/lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ devquiz/lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -1,7 +1,4 @@
 import 'package:devquiz/core/app_colors.dart';
-import 'package:devquiz/core/app_images.dart';
-import 'package:devquiz/core/app_text_styles.dart';
-import 'package:devquiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
@@ -10,7 +7,6 @@ class QuizCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(16),
       decoration: BoxDecoration(
         border: Border.fromBorderSide(
           BorderSide(
@@ -19,43 +15,6 @@ class QuizCardWidget extends StatelessWidget {
         ),
         color: AppColors.white,
         borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 40,
-            width: 40,
-            child: Image.asset(AppImages.blocks),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            "Geren ciamento de Estado",
-            style: AppTextStyles.heading15,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            children: [
-              Expanded(
-                flex: 1,
-                child: Text(
-                  "3/10",
-                  style: AppTextStyles.body11,
-                ),
-              ),
-              Expanded(
-                flex: 4,
-                child: ProgressIndicatorWidget(
-                  value: 0.3,
-                ),
-              ),
-            ],
-          ),
-        ],
       ),
     );
   }
